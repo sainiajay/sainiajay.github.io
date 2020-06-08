563cc51341959ca0896a71c19ac1f5b777009639
sainiajay <saini.ajay172@gmail.com>
2020-06-08 23:18:12 +0530

    Add Style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Ajay Saini
-      </header>
+      <div className="Nav-Links-Container">
+        <a className="Nav-Link" href="/#">
+          About Me
+        </a>
+        <a className="Nav-Link" href="/#">
+          Projects
+        </a>
+        <a className="Nav-Link" href="/#">
+          Work Experience
+        </a>
+        <a className="Nav-Link" href="/#">
+          Education
+        </a>
+      </div>
+      <h1 className="App-header">AJAY SAINI</h1>
+      <div className="Social-Links">
+        <i class="lni lni-linkedin" />
+        <i class="lni lni-github" />
+        <i class="lni lni-stackoverflow" />
+        <i class="lni lni-twitter" />
+        <i class="lni lni-medium" />
+      </div>
     </div>
   );
 }
