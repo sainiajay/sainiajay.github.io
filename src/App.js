dacb31ccef93130ca1992d38aeffa642b0674a95
sainiajay <saini.ajay172@gmail.com>
2020-06-30 11:46:30 +0530

    Change approach to min-width

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const sizes = {
 const media = Object.keys(sizes).reduce((accumulator, label) => {
   const emSize = sizes[label] / 16;
   accumulator[label] = (...args) => css`
-    @media (max-width: ${emSize}em) {
+    @media (min-width: ${emSize}em) {
       ${css(...args)};
     }
   `;
