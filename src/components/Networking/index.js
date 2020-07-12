b74c8a3a6cb1da5a3e6ffec6eabf6f27ea0eebe1
sainiajay <saini.ajay172@gmail.com>
2020-07-12 18:01:32 +0530

    Add Networking Links

--- src/components/Networking/index.js
+++ src/components/Networking/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const transitionTime = '0.1s';
 const StyledContainer = styled.div`
     position: fixed;
     width: 40px;
@@ -9,20 +10,24 @@ const StyledContainer = styled.div`
     top: calc(50vh - 120px);
 `;
 
-const Item = styled.i`
+const Item = styled.a`
   margin: 10px;
   font-size: 20px;
+  color: ${props => props.theme.color.text.primary};
   line-height: 2em;
   align-self: flex-end;
-  cursor: pointer;
+  &:hover {
+      transform: translateY(-5px);
+  }
+  transition: all ${transitionTime} ease-in-out;  
 `;
 
 const Networking = () => 
     <StyledContainer>
-        <Item className="lni lni-linkedin-original" />
-        <Item className="lni lni-github-original" />
-        <Item className="lni lni-twitter-original" />
-        <Item className="lni lni-medium" />
+        <Item className="lni lni-linkedin-original" href="https://www.linkedin.com/in/sainiajay96" target="_blank"/>
+        <Item className="lni lni-github-original" href="https://github.com/sainiajay" target="_blank"/>
+        <Item className="lni lni-twitter-original" href="https://twitter.com/sainiajay96" target="_blank"/>
+        <Item className="lni lni-medium" href="https://medium.com/@sainiajay" target="_blank"/>
     </StyledContainer>
 
 export default Networking;
