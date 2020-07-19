5f44386cd20fe9fec78e21c0a3021eb6f56105bf
sainiajay <saini.ajay172@gmail.com>
2020-07-19 12:33:04 +0530

    Remove Skills

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import withFrame from "../../commons/Frame/Frame";
 import { SectionHeader, Eyebrow, Heading } from "../../commons/Style";
-import { Skills } from "../Skills";
 import styled from "styled-components";
 import myImage from './../../assets/img/me.png';
 
@@ -27,6 +26,9 @@ const StyledContent = styled.div`
 const Body = styled.div`
     width: 60%;
     margin-right: 20px;
+    p {
+        font-size: 1.4em;
+    }
 `;
 
 const Image = styled.div`
@@ -56,7 +58,6 @@ const AboutMe = () => {
                     <p>I am Ajay Saini, a software engineer based in Bangalore, India.</p>
                     <p>I love creating softwares, whether that be websites, shopping portals, business applications, or anything in between. My passion for building software products brings up quality and amazing human experiences.</p>
                     <p>I began my coding journey in school and joined college to pursue engineering in Computer Science. Today, I work for with one of the Big-4s consulting firm.</p>
-                    <Skills />
                 </Body>
                 <Image>
                     <img src={myImage} alt="Ajay Saini" />
