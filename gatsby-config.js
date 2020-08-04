9b99f16a42a3e631ee24e6a928bc9d50bab1863f
sainiajay <saini.ajay172@gmail.com>
2020-08-04 11:18:15 +0530

    Setup data inject

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,28 @@
+const config = require('./src/config');
+
 module.exports = {
+    siteMetadata: {
+        title: config.siteTitle,
+        siteUrl: config.siteUrl,
+        description: config.siteDescription,
+    },
     plugins: [
         'gatsby-plugin-styled-components',
         'gatsby-plugin-fontawesome-css',
         'gatsby-plugin-react-helmet',
+        'gatsby-transformer-remark',
         {
             resolve: 'gatsby-plugin-scroll-reveal',
             options: {
                 once: false
             }
-        }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+              name: 'content',
+              path: `${__dirname}/content/`,
+            },
+        },
     ],
 }
