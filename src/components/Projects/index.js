7db003b78ac965ff4971d3781f57560cad6c4551
sainiajay <saini.ajay172@gmail.com>
2020-08-09 01:14:30 +0530

    Responsiveness fixes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,7 @@ import withFrame from '../../commons/Frame/Frame';
 import blackbox from "../../assets/img/blackbox.png";
 import pathfinder from "../../assets/img/pathfinder.png";
 import leetcode from "../../assets/img/leetcode.png";
+import { media } from "../../commons/StyleUtils";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
@@ -17,34 +18,54 @@ const StyledContainer = styled.div`
 
 const StyledContent = styled.div`
   position: relative;
-  grid-column: 1 / 7;
-  grid-row: 1 / -1;
-  padding: 40px 40px 30px;
-`;
+  grid-column: 2 / -2;
+  grid-row: 2 / -1;
+  z-index: 2;
 
+  ${media.tablet`
+    grid-column: 1 / 10;
+    grid-row: 1 / -1;
+    padding: 20px 20px 15px;
+  `}
+
+  ${media.desktop`
+    grid-column: 1 / 7;
+    grid-row: 1 / -1;
+    padding: 40px 40px 30px;
+  `}
+`;
+ 
 const StyledLabel = styled.h4`
   font-size: 12px;
   font-weight: normal;
-  color: ${props => props.theme.color.text.secondary};
   font-family: monospace;
-  margin-top: 10px;
+  margin-top: 1em;
   padding-top: 0;
+  color: #fff;
+  ${media.tablet`
+    color: ${props => props.theme.color.text.secondary};
+  `}
 `;
 
 const StyledProjectName = styled.h5`
-  font-size: 28px;
-  margin: 0 0 20px;
-  color: ${props => props.theme.color.text.primary};
+  font-size: 2.2em;
+  margin: 0 0 1em;
+  color: #fff;
+  ${media.tablet`
+    color: #000;
+  `}
 `;
 
 const StyledDescription = styled.div`
   position: relative;
   z-index: 2;
-  padding: 25px;
+  padding: 1em 2em;
+  line-height: 1.7em;
   background-color: ${props => props.theme.color.bg.secondary};
   color: ${props => props.theme.color.text.primary};
+  font-size: 1.1em;
   font-family: ${props => props.theme.font.secondary};
-  font-size: 14px;
+  text-align: left;
   border-radius: 4px;
   box-shadow: 0 5px 25px -20px #050505;
 `;
@@ -55,14 +76,13 @@ const StyledTechList = styled.ul`
   display: flex;
   flex-wrap: wrap;
   padding: 0;
-  margin: 25px 0 10px;
+  margin: 2em 0 1em;
   list-style: none;
   li {
-    font-size: 12px;
+    font-size: 1.1em;
     color: ${props => props.theme.color.text.secondary};
-    margin-right: 5px;
-    margin-bottom: 7px;
     white-space: nowrap;
+    margin-right: 1em;
     &:last-of-type {
       margin-right: 0;
     }
@@ -73,15 +93,14 @@ const StyledLinkWrapper = styled.div`
   display: flex;
   align-items: center;
   position: relative;
-  margin-top: 10px;
-  margin-left: -10px;
+  margin-left: -1em;
   a {
-    color: ${props => props.theme.color.text.primary};
+    color: #000;
     font-size: 1.5em;
-    padding: 10px;
+    padding: 1em;
     svg {
-      width: 22px;
-      height: 22px;
+      width: 2em;
+      height: 2em;
     }
   }
 `;
@@ -94,14 +113,21 @@ const StyledFeaturedImg = styled.img`
   position: relative;
   mix-blend-mode: multiply;
   filter: grayscale(100%) contrast(1) brightness(90%);
-  box-shadow: 1px 1px 5px 1px #eee;
 `;
 
 const StyledImgContainer = styled.a`
-  grid-column: 6 / -1;
-  grid-row: 1 / -1;
+  z-index: 1;
+  grid-column: 1 / -1;
+  grid-row: 1 / 3;
+  ${media.tablet`
+    grid-column: 4 / -1;
+    grid-row: 1 / -1;
+  `}
+  ${media.desktop`
+    grid-column: 6 / -1;
+    grid-row: 1 / -1;
+  `}
   position: relative;
-  border-radius: 5px;
   &:hover,
   &:focus {
     background: transparent;
@@ -121,6 +147,11 @@ const StyledImgContainer = styled.a`
     right: 0;
     bottom: 0;
     mix-blend-mode: screen;
+    border-radius: 4px;
+    background: rgba(0, 0, 0, 60%);
+    ${media.tablet`
+      background: none;
+    `}
   }
 `;
 
@@ -128,6 +159,7 @@ const StyledProject = styled.div`
   display: grid;
   grid-gap: 10px;
   grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(3, 1fr);
   align-items: center;
   margin-bottom: 100px;
   &:last-of-type {
@@ -135,23 +167,32 @@ const StyledProject = styled.div`
   }
   &:nth-of-type(odd) {
     ${StyledContent} {
-      grid-column: 7 / -1;
+      justify-content: flex-end;
       text-align: right;
+      ${media.tablet`
+        grid-column: 4 / -1;
+      `}
+      ${media.desktop`
+        grid-column: 7 / -1;
+      `}
     }
     ${StyledTechList} {
       justify-content: flex-end;
-      li {
-        margin-left: 10px;
-        margin-right: 0;
-      }
     }
     ${StyledLinkWrapper} {
       justify-content: flex-end;
-      margin-left: 0;
-      margin-right: -10px;
+      ${media.tablet`
+        margin-left: 0;
+        margin-right: -1em;
+      `}
     }
     ${StyledImgContainer} {
-      grid-column: 1 / 8;
+      ${media.tablet`
+        grid-column: 1 / 10;
+      `}
+      ${media.desktop`
+        grid-column: 1 / 8;
+      `}
     }
   }
 `;
@@ -212,8 +253,9 @@ const Featured = ({ data }) => {
                 </StyledContent>
 
                 <StyledImgContainer
-                  href={url ? URLSearchParams : github ? github : '#'}
+                  href={url || github || '#'}
                   target="_blank"
+                  data-image={image}
                   rel="nofollow noopener noreferrer">
                   <StyledFeaturedImg src={image} alt={title} />
                 </StyledImgContainer>
